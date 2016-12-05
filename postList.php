8a02f38c08908847fbbefe46b9e11836db3796aa
Jess Tan <jess_tjl@hotmail.com>
2016-12-05 23:51:49 +0800

    cdn datable for postlist

--- postList.php
+++ postList.php
@@ -6,10 +6,22 @@
 ?>
 <html lang="en">
   <?php head("Dear Carrie - Admin Post List"); ?>
-  <script src="css/jquery.dataTables.min.css"></script>
-  <script src="css/dataTables.bootstrap.min.css"></script>
-  <script src="js/jquery.dataTables.min.js"></script>
-  <script src="js/dataTables.bootstrap.min.js"></script>
+  <!--<script src="css/jquery.dataTables.min.css"></script>-->
+  <!--<script src="css/dataTables.bootstrap.min.css"></script>-->
+  <!--<script src="js/jquery.dataTables.min.js"></script>-->
+  <!--<script src="js/dataTables.bootstrap.min.js"></script>-->
+
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.12/datatables.min.css"/>
+  <script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.12/datatables.min.js"></script>
+  <style>
+  /* hard overwrite */
+  .form-control {
+    height: 30px !important;
+    border-radius: 0;
+    letter-spacing: 1px;
+    margin: 0 5px;
+  }
+  </style>
 
   <body>
     <?= navbar(); ?>
@@ -20,61 +32,37 @@
             <li><a href="dashboard">Dashboard</a></li>
             <li class="active"><a href="#">Posts</a></li>
         </ol>
-        <div class="panel panel-default summary-panel">
-            <div class="panel-heading">
-                <h4 class="admin-sec-color">Total Posts: 3455</h4>
-            </div>
-            <table id="post-list" class="table table-hover table-bordered table-striped">
-                <thead>
-                <tr>
-                    <th>Post ID</th>
-                    <th>Date</th>
-                    <th>Title</th>
-                    <th>User ID</th>
-                    <th>Likes</th>
-                    <th>Comment</th>
-                    <th></th>
-                </tr>
-                </thead>
 
-                <tbody>
-                <?php foreach ($posts as $post) { ?>
-                <tr>
-                    <td><?=$post["id"];?></td>
-                    <td><?=$post["timestamp"];?></td>
-                    <!-- remember to do a substring function to keep the text to one line -->
-                    <td><?=$post["title"];?></td>
-                    <td><a href="userDetails?userID=<?=$post["userid"];?>"><?=$post["userid"];?></a></td>
-                    <td>43</td>
-                    <td>10</td>
-                    <td><a href="postDetails?postID=<?=$post["id"];?>" class="admin-sec-color">View</a></td>
-                </tr>
-                <?php } ?>
-                </tbody>
-            </table>
-        </div><!-- END post table-->
+        <table id="post-list" class="table table-hover table-bordered table-striped">
+            <thead>
+            <tr>
+                <th>Post ID</th>
+                <th>Date</th>
+                <th>Title</th>
+                <th>User ID</th>
+                <th>Likes</th>
+                <th>Comment</th>
+                <th></th>
+            </tr>
+            </thead>
 
-        <nav aria-label="Page navigation">
-            <center>
-              <ul class="pagination">
-                  <li>
-                      <a href="#" aria-label="Previous">
-                          <span aria-hidden="true">&laquo;</span>
-                      </a>
-                  </li>
-                  <li><a href="#">1</a></li>
-                  <li><a href="#">2</a></li>
-                  <li><a href="#">3</a></li>
-                  <li><a href="#">4</a></li>
-                  <li><a href="#">5</a></li>
-                  <li>
-                      <a href="#" aria-label="Next">
-                          <span aria-hidden="true">&raquo;</span>
-                      </a>
-                  </li>
-              </ul>
-            </center>
-        </nav>
+            <tbody>
+            <?php foreach ($posts as $post) { ?>
+            <tr>
+                <td><?=$post["id"];?></td>
+                <td><?=$post["timestamp"];?></td>
+                <!-- remember to do a substring function to keep the text to one line -->
+                <td><?=$post["title"];?></td>
+                <td><a href="userDetails?userID=<?=$post["userid"];?>"><?=$post["userid"];?></a></td>
+                <td>43</td>
+                <td>10</td>
+                <td><a href="postDetails?postID=<?=$post["id"];?>" class="admin-sec-color">View</a></td>
+            </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+
+      
     </div><!-- ./page-container -->
 
     <?php footer(); ?>
