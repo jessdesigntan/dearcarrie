b3335915d350bb1ecfdfd0086427938a8968d1fa
Jess Tan <jess_tjl@hotmail.com>
2016-12-09 14:51:42 +0800

    refactor

--- suggestions.php
+++ suggestions.php
@@ -1,7 +1,6 @@
 <?php error_reporting(0);
- 
-$con = mysql_connect('localhost', 'root','root');
-$db = mysql_select_db('dearcarrie');
+include("controllers/templates.php");
+$con = connectToDataBase();
 //where 'term' is the default keyword in jquery autocomplete api
 $query = $_REQUEST['term'];
 $sql = "SELECT * FROM posts WHERE (title LIKE '%$query%' OR description LIKE '%$query%' OR timestamp LIKE '%$query%') AND published = 1";
@@ -12,5 +11,5 @@ $val[] = $row['title'];
 }
 //here we convert the result into JSON
 echo json_encode($val);
- 
+
 ?>
