806fd37bb342dca72a5f5853c37060ea2713cc00
Jess Tan <jess_tjl@hotmail.com>
2016-12-11 00:27:11 +0800

    empty state for no search results and sort by date

--- search.php
+++ search.php
@@ -19,13 +19,28 @@
             <?= sideFilter(); ?>
           </div>
         </div><!-- END right column col-sm-3 -->
-       
-        
+
+
         <div class="col-sm-8">
           <div class="content-title">
             <h4>Search Results: <span class="primary-color"><?=$keyword;?></span></h4>
-            <a href="#">Sort by Relevance &#9662;</a>
+            <a class="dropdown">Sort by &#9662;
+                <ul>
+                    <li>Today</li>
+                    <li>This month</li>
+                    <li>This year</li>
+                </ul>
+            </a>
           </div>
+          <!-- do an if-else for posts count -->
+          <div class="post-empty-state">
+              <div>
+                  <h4>No posts found</h4>
+              </div>
+          </div>
+          <!-- /do an if-else for posts count -->
+
+
           <?php
             if ($searchOption == "suggestions") {
                 $posts = searchPost($keyword);
