ec6f2bf71871239f8c94d0ae9cb0ca7b8ede643c
Jess Tan <jess_tjl@hotmail.com>
2016-12-11 02:10:16 +0800

    notification trigger

--- sql/dearcarrie.sql
+++ sql/dearcarrie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 04, 2016 at 01:58 PM
+-- Generation Time: Dec 10, 2016 at 07:09 PM
 -- Server version: 5.6.28
 -- PHP Version: 7.0.10
 
@@ -40,13 +40,24 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id`, `userid`, `postid`, `datetime`, `likes`, `comment`) VALUES
-(2, 30, 13, '2016-12-02 16:32:43', 0, 'hohoho'),
-(3, 30, 7, '2016-12-02 16:43:07', 0, 'new comment'),
 (4, 30, 7, '2016-12-02 16:43:15', 0, 'hahaha'),
 (5, 30, 7, '2016-12-02 16:43:22', 0, 'test scroll'),
-(6, 30, 13, '2016-12-02 16:45:33', 0, 'test'),
 (7, 38, 13, '2016-12-02 16:51:54', 0, 'normal comment here'),
-(8, 30, 13, '2016-12-04 11:39:43', 0, 'test');
+(10, 30, 6, '2016-12-07 15:09:07', 0, 'haha');
+
+--
+-- Triggers `comments`
+--
+DELIMITER $$
+CREATE TRIGGER `comment_notification` AFTER INSERT ON `comments` FOR EACH ROW INSERT INTO notifications (item, from_user, type)
+VALUES (NEW.id, NEW.userid, 'new_comment')
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `delete_comment` AFTER DELETE ON `comments` FOR EACH ROW DELETE FROM notifications
+WHERE item = OLD.id AND from_user = OLD.userid AND type = 'new_comment'
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -57,8 +68,29 @@ INSERT INTO `comments` (`id`, `userid`, `postid`, `datetime`, `likes`, `comment`
 CREATE TABLE `comment_like` (
   `commentid` int(11) NOT NULL,
   `userid` int(11) NOT NULL,
-  `datetime` int(11) NOT NULL
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `comment_like`
+--
+
+INSERT INTO `comment_like` (`commentid`, `userid`, `datetime`) VALUES
+(4, 30, '2016-12-07 18:02:12');
+
+--
+-- Triggers `comment_like`
+--
+DELIMITER $$
+CREATE TRIGGER `comment_like_notification` AFTER INSERT ON `comment_like` FOR EACH ROW INSERT INTO notifications (item, from_user, type)
+VALUES (NEW.commentid, NEW.userid, 'comment_like')
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `comment_unlike_notification` AFTER DELETE ON `comment_like` FOR EACH ROW DELETE FROM notifications
+WHERE item = OLD.commentid AND from_user = OLD.userid AND type = 'comment_like'
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -81,8 +113,22 @@ INSERT INTO `curation` (`topicid`, `postid`) VALUES
 (11, 3),
 (12, 3),
 (10, 3),
-(12, 13),
-(10, 13);
+(10, 13),
+(9, 13),
+(12, 13);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `item` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `seen` int(11) NOT NULL DEFAULT '0',
+  `from_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -119,7 +165,33 @@ INSERT INTO `posts` (`id`, `userid`, `title`, `description`, `timestamp`, `score
 (10, 30, 'Title here', 'this is my story<br />\r\n<br />\r\nspace<br />\r\n<br />\r\none more time<br />\r\ntesting', '2016-11-30 12:00:30', 0, 0, 0, 0, 0),
 (11, 30, 'hahahaha', 'lolololool', '2016-11-30 11:57:16', 0, 0, 0, 0, 0),
 (12, 30, 'ok', 'change again', '2016-11-30 11:55:55', 0, 0, 0, 0, 0),
-(13, 33, 'New title', 'So lame', '2016-12-02 15:16:13', 0, 0, 0, 0, 1);
+(13, 33, 'New title', 'So lame', '2016-12-02 15:16:13', 0, 0, 0, 0, 1),
+(14, 30, 'bipolar test', 'alalalalalalla bipolar', '2016-12-08 15:58:47', 0, 0, 0, 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_follow`
+--
+
+CREATE TABLE `post_follow` (
+  `postid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Triggers `post_follow`
+--
+DELIMITER $$
+CREATE TRIGGER `post_follow_notification` AFTER INSERT ON `post_follow` FOR EACH ROW INSERT INTO notifications (item, from_user, type)
+VALUES (NEW.postid, NEW.userid, 'post_follow')
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `post_unfollow_notification` AFTER DELETE ON `post_follow` FOR EACH ROW DELETE FROM notifications
+WHERE item = OLD.postid AND from_user = OLD.userid AND type = 'post_follow'
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -130,8 +202,31 @@ INSERT INTO `posts` (`id`, `userid`, `title`, `description`, `timestamp`, `score
 CREATE TABLE `post_like` (
   `postid` int(11) NOT NULL,
   `userid` int(11) NOT NULL,
-  `datetime` datetime NOT NULL
+  `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `post_like`
+--
+
+INSERT INTO `post_like` (`postid`, `userid`, `datetime`) VALUES
+(3, 30, '2016-12-07 17:16:33'),
+(7, 30, '2016-12-07 18:17:24'),
+(6, 30, '2016-12-07 18:17:26');
+
+--
+-- Triggers `post_like`
+--
+DELIMITER $$
+CREATE TRIGGER `post_like_notifications` AFTER INSERT ON `post_like` FOR EACH ROW INSERT INTO notifications (item, from_user, type)
+VALUES (NEW.postid, NEW.userid, 'post_like')
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `post_unlike_notification` AFTER DELETE ON `post_like` FOR EACH ROW DELETE FROM notifications
+WHERE item = OLD.postid AND from_user = OLD.userid AND type = 'post_like'
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -176,11 +271,11 @@ CREATE TABLE `topics` (
 --
 
 INSERT INTO `topics` (`id`, `main_image`, `background`, `title`, `short_desc`, `description`, `date`, `followers`, `posts`, `url`, `tel`, `type`, `order_num`, `score`, `published`) VALUES
-(8, 'images/topics/4931553_eating.jpg', 'images/topics/5851188_foodbg.jpg', 'Eating Disorders', 'Stories about split personality', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', '2016-12-02 18:07:29', 0, 0, 'www.split.commmmmm', '12345678', 'featured', 5, 0, 0),
-(9, 'images/topics/9123645_split.jpg', 'images/topics/904009_split.jpg', 'Split Personality', 'Stories about split personality', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', '2016-12-02 18:04:37', 0, 0, 'www.split.com', '+65 8888 5555', 'curated', 1, 0, 0),
-(10, 'images/topics/1448827_eating.jpg', 'images/topics/6449401_eating.jpg', 'Stress & Studies', '123', '123', '2016-12-02 17:04:45', 0, 0, '123', '123', 'main', 2, 0, 1),
+(8, 'images/topics/4931553_eating.jpg', 'images/topics/5851188_foodbg.jpg', 'Eating Disorders', 'Stories about split personality', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', '2016-12-04 16:30:41', 0, 0, 'www.split.commmmmm', '12345678', 'featured', 5, 0, 1),
+(9, 'images/topics/9123645_split.jpg', 'images/topics/904009_split.jpg', 'Split Personality', 'Stories about split personality', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis,', '2016-12-04 16:30:43', 0, 0, 'www.split.com', '+65 8888 5555', 'curated', 1, 0, 1),
+(10, 'images/topics/9896764_love.jpg', 'images/topics/9629871_love.jpg', 'Love & Relationships', '123', '123', '2016-12-04 16:48:48', 0, 0, '123', '123', 'main', 2, 0, 1),
 (11, 'images/topics/9484689_split.jpg', 'images/topics/3974277_splitbg.jpg', 'Split Personality', 'Stories about split personality', 'A collection of stories about split personality', '2016-12-02 18:06:19', 0, 0, 'www.split.com', '1234 5678', 'main', 1, 0, 1),
-(12, 'images/topics/118669_stress.jpg', 'images/topics/8314051_stress.jpg', 'Stress', 'Tips to deal with stress', 'Stress & studies', '2016-12-02 17:21:13', 0, 0, 'http://www.stress.com', '5678 8888', 'main', 3, 0, 1);
+(12, 'images/topics/118669_stress.jpg', 'images/topics/1149696_stressbg.jpg', 'Stress', 'Tips to deal with stress', 'Stress & studies', '2016-12-04 16:46:23', 0, 0, 'http://www.stress.com', '5678 8888', 'main', 3, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -199,9 +294,7 @@ CREATE TABLE `topic_follow` (
 --
 
 INSERT INTO `topic_follow` (`userid`, `topicid`, `datetime`) VALUES
-(30, 11, '2016-12-04 11:22:22'),
-(30, 12, '2016-12-04 11:22:29'),
-(30, 9, '2016-12-04 11:27:52');
+(30, 9, '2016-12-10 15:19:40');
 
 -- --------------------------------------------------------
 
@@ -227,14 +320,47 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `affliate`, `image`, `description`, `datejoin`, `active`) VALUES
-(30, 'Jess Tan', 'jess_tjl@hotmail.com', '202cb962ac59075b964b07152d234b70', 'admin', 'website', 'images/profile/3393592_eating.jpg', 'I love writing', '2016-12-01 08:49:41', 1),
+(30, 'Jess Tan', 'jess_tjl@hotmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'admin', 'website', 'images/default.svg', 'I love writing', '2016-12-01 08:49:41', 1),
 (32, 'normal', 'normal@mail.com', '202cb962ac59075b964b07152d234b70', 'normal', 'website', 'images/profile/5917602_dots.svg', 'im cool', '0000-00-00 00:00:00', 0),
 (33, 'New Name', 'newuser@mail.com', '202cb962ac59075b964b07152d234b70', 'normal', 'website', 'images/profile/5947141_dots.svg', 'New description', '0000-00-00 00:00:00', 1),
 (34, 'Tan Jia Lin', 'jesswandering@gmail.com', '202cb962ac59075b964b07152d234b70', 'normal', 'website', 'images/default.svg', 'lololololol', '0000-00-00 00:00:00', 1),
 (35, 'Claudia', 'test@mail.commmmm', '202cb962ac59075b964b07152d234b70', 'normal', 'website', 'images/default.svg', 'lalalal', '0000-00-00 00:00:00', 1),
 (36, 'Tan Jia Lin', 'tjl@gmail.com', '202cb962ac59075b964b07152d234b70', 'normal', 'website', 'images/default.svg', '', '2016-12-01 08:49:41', 1),
 (37, 'testdate', 'testdate@mail.com', '202cb962ac59075b964b07152d234b70', 'normal', 'website', 'images/default.svg', '', '2016-12-02 11:24:44', 1),
-(38, 'normal user', 'normaluser@mail.com', '202cb962ac59075b964b07152d234b70', 'normal', 'website', 'images/default.svg', '', '2016-12-02 16:51:45', 1);
+(38, 'normal user', 'normaluser@mail.com', '202cb962ac59075b964b07152d234b70', 'normal', 'website', 'images/default.svg', '', '2016-12-02 16:51:45', 1),
+(39, 'testprofile', 'prof@mail.com', '202cb962ac59075b964b07152d234b70', 'normal', 'website', 'images/default.svg', '', '2016-12-10 15:59:12', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_follow`
+--
+
+CREATE TABLE `user_follow` (
+  `userid` int(11) NOT NULL,
+  `follower` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_follow`
+--
+
+INSERT INTO `user_follow` (`userid`, `follower`) VALUES
+(33, 30);
+
+--
+-- Triggers `user_follow`
+--
+DELIMITER $$
+CREATE TRIGGER `user_follow_notification` AFTER INSERT ON `user_follow` FOR EACH ROW INSERT INTO notifications (item, from_user, type)
+VALUES (NEW.userid, NEW.follower, 'user_follow')
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `user_unfollow_notification` AFTER DELETE ON `user_follow` FOR EACH ROW DELETE FROM notifications
+WHERE item = OLD.userid AND from_user = OLD.follower AND type = 'user_follow'
+$$
+DELIMITER ;
 
 --
 -- Indexes for dumped tables
@@ -279,12 +405,12 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `reports`
 --
@@ -299,7 +425,7 @@ ALTER TABLE `topics`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
