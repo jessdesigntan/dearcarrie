c4302771e0c8af31ac5afb071bf6c3b6067289e3
Jess Tan <jess_tjl@hotmail.com>
2017-03-08 14:01:07 +0800

    refactor comment like logic

--- js/likeComment.js
+++ js/likeComment.js
@@ -1,36 +1,12 @@
-var likeBtn = "Like Comment";
-var unlikeBtn = "Liked";
 
-function likeComment(ele, userid, commentid) {
-    if (likeBtn == "Liked") {
-        $('#'+ele).removeClass("active");
-        unlikeComment(ele, userid, commentid);
-        return;
-    }
-    if (window.XMLHttpRequest) {
-        // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            likeBtn = this.responseText.trim();
-            unlikeBtn = this.responseText.trim();
-            $('#'+ele).addClass("active");
-        }
-    };
-    xmlhttp.open("GET","likeFunctions?userid="+userid+"&commentid="+commentid+"&action=likecomment",true);
-    xmlhttp.send();
+function returnClass(ele) {
+     return document.getElementById(ele).className;
 }
 
-function unlikeComment(ele, userid, commentid) {
-    if (unlikeBtn == "Like Comment") {
-        $('#'+ele).addClass("active");
-        likeComment(ele, userid, commentid);
-        return;
-    }
+
+function likeComment(ele, userid, commentid) {
+  var className = returnClass(ele);
+  if (className == "star-icon active") {
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp = new XMLHttpRequest();
@@ -47,4 +23,60 @@ function unlikeComment(ele, userid, commentid) {
     };
     xmlhttp.open("GET","likeFunctions?userid="+userid+"&commentid="+commentid+"&action=unlikecomment",true);
     xmlhttp.send();
+  }
+  else {
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            $('#'+ele).addClass("active");
+        }
+    };
+    xmlhttp.open("GET","likeFunctions?userid="+userid+"&commentid="+commentid+"&action=likecomment",true);
+    xmlhttp.send();
+  }
+}
+
+function unlikeComment(ele, userid, commentid) {
+  var className = returnClass(ele);
+  if (className == "star-icon active") {
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            likeBtn = this.responseText.trim();
+            unlikeBtn = this.responseText.trim();
+            $('#'+ele).removeClass("active");
+        }
+    };
+    xmlhttp.open("GET","likeFunctions?userid="+userid+"&commentid="+commentid+"&action=unlikecomment",true);
+    xmlhttp.send();
+  }
+  else {
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            $('#'+ele).addClass("active");
+        }
+    };
+    xmlhttp.open("GET","likeFunctions?userid="+userid+"&commentid="+commentid+"&action=likecomment",true);
+    xmlhttp.send();
+  }
+
 }
